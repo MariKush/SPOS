6df3d80716f16e9001652766e717ce6d041fd1f7
MariKush <43176370+MariKush@users.noreply.github.com>
2019-11-19 20:04:33 +0200

    smashed it into folders for lab work and made the code more beautiful

--- lab3/my_package/SchedulingAlgorithm.java
+++ lab3/my_package/SchedulingAlgorithm.java
@@ -13,26 +13,26 @@ public class SchedulingAlgorithm {
   static Random random;
 
   static{
-    random=new Random(System.currentTimeMillis());;
+    random = new Random(System.currentTimeMillis());;
   }
 
   private static int nextProcess( Vector<sProcess> processVector, int blocked){
 
-    Vector<Integer> process=new Vector<Integer>();
-    int size=processVector.size();
-    for(int i=0;i<size;i++){
-      if(i==blocked)
+    Vector<Integer> process = new Vector<Integer>();
+    int size = processVector.size();
+    for(int i = 0; i < size; i++){
+      if(i == blocked)
         continue;
       if(processVector.elementAt(i).cpudone!=processVector.elementAt(i).cputime)
         process.addElement(i);
     }
-    if (process.size()==0)process.addElement(blocked);
+    if (process.size() == 0) process.addElement(blocked);
 
-    size=process.size();
-    int countTicket=10*size;
-    int []ticket=new int[countTicket];
-    for(int i=0;i<countTicket;i++){
-      ticket[i]=process.elementAt(i%size);
+    size = process.size();
+    int countTicket = 10*size;
+    int []ticket = new int[countTicket];
+    for(int i = 0; i < countTicket; i++){
+      ticket[i] = process.elementAt(i%size);
     }
     return ticket[random.nextInt(countTicket)];
 
@@ -42,43 +42,42 @@ public class SchedulingAlgorithm {
     int i = 0;
     int comptime = 0;
     int currentProcess = 0;
-    int previousProcess = 0;
     int size = processVector.size();
     int completed = 0;
     String resultsFile = "Summary-Processes";
 
     result.schedulingType = "Batch (Nonpreemptive)";
-    result.schedulingName = "First-Come First-Served"; 
+    result.schedulingName = "Lottery"; 
     try {
       //BufferedWriter out = new BufferedWriter(new FileWriter(resultsFile));
       //OutputStream out = new FileOutputStream(resultsFile);
       PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
       sProcess process = processVector.elementAt(currentProcess);
-      out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+      out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + ")");
       while (comptime < runtime) {
         if (process.cpudone == process.cputime) {
           completed++;
-          out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+          out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + ")");
           if (completed == size) {
             result.compuTime = comptime;
             out.close();
             return result;
           }
 
-          currentProcess=nextProcess(processVector, -1);
+          currentProcess = nextProcess(processVector, -1);
 
           process = processVector.elementAt(currentProcess);
-          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + ")");
         }      
         if (process.ioblocking == process.ionext) {
-          out.println("Process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+          out.println("Process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + ")");
           process.numblocked++;
           process.ionext = 0; 
           
-          currentProcess=nextProcess(processVector, currentProcess);
+          currentProcess = nextProcess(processVector, currentProcess);
 
           process = processVector.elementAt(currentProcess);
-          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + ")");
         }        
         process.cpudone++;       
         if (process.ioblocking > 0) {
